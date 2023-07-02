f8ac8adf9f92d29e50fdc2547d13c468d0ab37cb
abhijeet <pabhi9156@gmail.com>
2023-07-02 11:50:13 +0530

    Remove file

---
+++ gitTut.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+// <bits / stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a =  122;
+    b = 8;
+    c= a+b;
+    cout << c ;
+
+    return 0;
+}
